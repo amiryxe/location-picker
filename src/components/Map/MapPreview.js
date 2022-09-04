57249e2f80e43b16bb38d6eada7ec1d2e9a9a88e
Amir Salehi <nik.amirsa@gmail.com>
2022-09-04 09:15:04 +0430

    changes

--- src/components/Map/MapPreview.js
+++ src/components/Map/MapPreview.js
@@ -1,25 +1,25 @@
-import { useContext } from 'react';
-import MainContext from '../../context/mainContext';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import data from '../../util/Data';
-import './MapPreview.scss';
-import L from 'leaflet';
-delete L.Icon.Default.prototype._getIconUrl;
+import { useContext } from 'react'
+import MainContext from '../../context/mainContext'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import data from '../../util/Data'
+import './MapPreview.scss'
+import L from 'leaflet'
+delete L.Icon.Default.prototype._getIconUrl
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+})
 
-const MapPreview = () => {
-  const { currentPosition, setActive, active } = useContext(MainContext);
+export default function MapPreview() {
+  const { currentPosition, setActive, active } = useContext(MainContext)
 
   const clickMarkerHandler = (id) => {
-    setActive(id);
-    let item = document.getElementById('item' + id);
-    item.scrollIntoView();
-  };
+    setActive(id)
+    let item = document.getElementById('item' + id)
+    item.scrollIntoView()
+  }
 
   return (
     <MapContainer
@@ -30,7 +30,7 @@ const MapPreview = () => {
     >
       <TileLayer
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
 
       {data.map((item) => (
@@ -44,7 +44,5 @@ const MapPreview = () => {
         </Marker>
       ))}
     </MapContainer>
-  );
-};
-
-export default MapPreview;
+  )
+}
