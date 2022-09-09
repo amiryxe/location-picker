6f54572ce77262a880dab92a7278a3cc2f1efb5e
Amir Salehi <nik.amirsa@gmail.com>
2022-09-09 13:17:50 +0430

    unistall vazir font old package and install vazirmatn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import MapPreview from './components/Map/MapPreview'
 import List from './components/List/List'
 import Info from './components/Info'
 import MainState from './context/MainState'
-import 'vazir-font/dist/font-face.css'
+import 'vazirmatn/dist/font-face.css'
 import 'leaflet/dist/leaflet.css'
 import './App.css'
 
